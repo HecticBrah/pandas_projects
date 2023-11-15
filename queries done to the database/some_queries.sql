a892345acabe4e374c2ee11343ed14d6d1c0e749
Dominik Vukelić <76909711+HecticBrah@users.noreply.github.com>
2023-11-15 18:48:21 +0100

    Update some_queries.sql

--- queries done to the database/some_queries.sql
+++ queries done to the database/some_queries.sql
@@ -80,3 +80,7 @@ SELECT driver_race, COUNT(*) AS DifferentRaces FROM police_data GROUP BY driver_
 --Adding a new column with just the date
 ALTER TABLE police_data
 ADD just_date VARCHAR(10); -- Adjust the VARCHAR length to accommodate the new format
+
+--Setting values of a new column by getting the values from original stop_date_and_time column which was in varchar 100 format and converting it to 101 format
+UPDATE police_data
+SET just_date = CONVERT(VARCHAR, CONVERT(DATE, stop_date_and_time, 100), 101);
