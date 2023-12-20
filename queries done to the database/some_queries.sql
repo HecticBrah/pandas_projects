fa3b72bdc3b82b45802770a6a3005a7d8470f156
Dominik Vukelic <letotalhavoc@gmail.com>
2023-12-20 09:51:11 +0100

    added a query

--- queries done to the database/some_queries.sql
+++ queries done to the database/some_queries.sql
@@ -175,3 +175,13 @@ FROM (
 
 --Day of the year for all stops in a table
 SELECT DATEPART(DAYOFYEAR, stop_date_and_time) AS day_of_year FROM police_data;
+
+--Get all information about a table
+SELECT
+name AS column_name,
+TYPE_NAME(system_type_id) AS column_type,
+max_length,
+collation_name,
+is_nullable
+FROM sys.columns
+WHERE object_id = OBJECT_ID(N'police_data');
